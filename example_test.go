7a19bdb20b197a158ac6719067f9f7ac92928b3a
Larz Conwell <larzconwell@gmail.com>
2014-05-03 22:50:41 -0400

    Add example.

---
+++ example_test.go
@@ -0,0 +1,41 @@
+package ar_test
+
+import (
+  "github.com/larzconwell/ar"
+  "io"
+  "os"
+)
+
+func Example() {
+  in, err := os.Open("libz.a")
+  if err != nil {
+    panic(err)
+  }
+  defer in.Close()
+  arReader := ar.NewReader(in)
+
+  for {
+    header, err := arReader.Next()
+    if err != nil {
+      panic(err)
+    }
+    if header == nil {
+      break
+    }
+
+    out, err := os.Create(header.Name)
+    if err != nil {
+      panic(err)
+    }
+
+    _, err = io.Copy(out, arReader)
+    if err != nil {
+      panic(err)
+    }
+
+    err = out.Close()
+    if err != nil {
+      panic(err)
+    }
+  }
+}
